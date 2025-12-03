e61551717e9cdd8d1fd373dabcfd99a769350929
Anis Afifi <anis@webxbeyond.com>
2025-12-03 14:04:32 +0600

    hide mintlify

---
+++ hide.js
@@ -0,0 +1,15 @@
+const replaceMintlify = () => {
+  const el = document.querySelector('a[href*="mintlify"]');
+  if (el) {
+    el.href = "https://hixbe.com";
+    el.innerHTML = `Powered by <strong>Hixbe</strong>`;
+  }
+};
+
+replaceMintlify();
+
+// If Mintlify re-hydrates, run again
+new MutationObserver(replaceMintlify).observe(document.body, {
+  childList: true,
+  subtree: true
+});
